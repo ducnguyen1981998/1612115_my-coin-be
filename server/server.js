d8f76328aa2cc471ce4c173a4f33f3f36a708ccb
1981998 <44722881+ducnguyen1981998@users.noreply.github.com>
2021-05-11 22:06:59 +0700

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const path= require('path');
+const express = require('express');
+
+
+// console.log(__dirname+ "/../public");
+const publicPath = path.join(__dirname, '/../public');
+const port = process.env.PORT || 3000;
+var app = express();
+
+
+app.use(express.static(publicPath));
+
+app.listen(port, ()=>{
+    console.log(`Server is listening in port ${port}`);
+});
