f09ca718387e83db7b7b4a37d1975cb1626a3ec1
1981998 <44722881+ducnguyen1981998@users.noreply.github.com>
2021-05-11 22:43:45 +0700

    Setup for pj
    + Add express, socket
    + Socket.IO

--- server/server.js
+++ server/server.js
@@ -1,15 +1,43 @@
-const path= require('path');
+// const path= require('path');
+// const express = require('express');
+// const http = require('http');
+// const socketIO = require('socket.io')
+
+
+// // console.log(__dirname+ "/../public");
+// const publicPath = path.join(__dirname, '/../public');
+// const port = process.env.PORT || 3000;
+// let app = express();
+// let server = http.createServer(app);
+// let io = socketIO(server);
+
+// app.use(express.static(publicPath));
+
+
+
+const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
-
-// console.log(__dirname+ "/../public");
 const publicPath = path.join(__dirname, '/../public');
 const port = process.env.PORT || 3000;
-var app = express();
-
+let app = express();
+let server = http.createServer(app);
+let io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, ()=>{
-    console.log(`Server is listening in port ${port}`);
+//Connect
+io.on('connection', (socket) => {
+    console.log("A new user just connected");
 });
+
+//Disconnect
+socket.on('disconnect', () => {
+    console.log("A new user just disconnected");
+});
+  
+server.listen(port, ()=>{
+console.log(`Server is up on port ${port}`);
+})
